c2591c60e84fe0136dede4560486f4fdfe1f6e43
HabidRC <habidrdzcont@outlook.com>
2026-08-24 08:28:49 -0600

    Añadiendo botones

--- Piedra_Papel_Tijeras/Form1.Designer.cs
+++ Piedra_Papel_Tijeras/Form1.Designer.cs
@@ -28,20 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(110, 142);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 95);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "PIEDRA";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(234, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 95);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "PAPEL";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(358, 142);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 95);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "TIJERA";
+            this.button3.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1057, 564);
+            this.ClientSize = new System.Drawing.Size(1586, 868);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Menú Principal";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
