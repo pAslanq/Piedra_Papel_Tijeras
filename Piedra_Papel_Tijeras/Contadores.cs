fcdfc8ce2e9a685ac134e629bd9f36106d4ca8b0
pAslanq <aaslan.git@gmail.com>
2026-09-01 08:58:36 -0600

    var Total aprendizajes

--- Piedra_Papel_Tijeras/Contadores.cs
+++ Piedra_Papel_Tijeras/Contadores.cs
@@ -8,6 +8,8 @@ namespace Piedra_Papel_Tijeras
 {
     public class Contadores
     {
+        public int Total_Aprendizaje { get; set; } = 0;
+
         public int Piedra_Piedra { get; set; } = 0;
         public int Piedra_Papel { get; set; } = 0;
         public int Piedra_Tijera { get; set; } = 0;
