4dd0736d9016c62cf2ac21a62c93e912c3fd78be
Ken <sabs355346@gmail.com>
2026-09-08 19:33:37 -0600

    Mostrar Base de Datos

---
+++ Piedra_Papel_Tijeras/Form3.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace Piedra_Papel_Tijeras
+{
+    partial class Form3
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.Sidebar = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.MenuButton = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.Sidebar_Timer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelTitulos = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panelTabla = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panelContenido = new System.Windows.Forms.Panel();
+            this.Sidebar.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MenuButton)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelTitulos.SuspendLayout();
+            this.panelTabla.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Sidebar
+            // 
+            this.Sidebar.BackColor = System.Drawing.Color.Gray;
+            this.Sidebar.Controls.Add(this.panel1);
+            this.Sidebar.Controls.Add(this.panel3);
+            this.Sidebar.Controls.Add(this.panel4);
+            this.Sidebar.Controls.Add(this.panel5);
+            this.Sidebar.Dock = System.Windows.Forms.DockStyle.Right;
+            this.Sidebar.FlowDirection = System.Windows.Forms.FlowDirection.BottomUp;
+            this.Sidebar.Location = new System.Drawing.Point(1321, 0);
+            this.Sidebar.MaximumSize = new System.Drawing.Size(247, 0);
+            this.Sidebar.MinimumSize = new System.Drawing.Size(70, 0);
+            this.Sidebar.Name = "Sidebar";
+            this.Sidebar.Size = new System.Drawing.Size(70, 760);
+            this.Sidebar.TabIndex = 0;
+            this.Sidebar.WrapContents = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.MenuButton);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(3, 681);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(244, 76);
+            this.panel1.TabIndex = 2;
+            // 
+            // MenuButton
+            // 
+            this.MenuButton.Image = global::Piedra_Papel_Tijeras.Properties.Resources.icons8_ajustes_48;
+            this.MenuButton.Location = new System.Drawing.Point(11, 14);
+            this.MenuButton.Name = "MenuButton";
+            this.MenuButton.Size = new System.Drawing.Size(56, 53);
+            this.MenuButton.TabIndex = 1;
+            this.MenuButton.TabStop = false;
+            this.MenuButton.Click += new System.EventHandler(this.MenuButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Cooper Black", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(80, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "AJUSTES";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Location = new System.Drawing.Point(3, 599);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(244, 76);
+            this.panel3.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Cooper Black", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.button3.Image = global::Piedra_Papel_Tijeras.Properties.Resources.icons8_chicle__48;
+            this.button3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button3.Location = new System.Drawing.Point(0, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(244, 76);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "ENTRENAR";
+            this.button3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.button4);
+            this.panel4.Location = new System.Drawing.Point(3, 517);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(244, 76);
+            this.panel4.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Font = new System.Drawing.Font("Cooper Black", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.Color.Yellow;
+            this.button4.Image = global::Piedra_Papel_Tijeras.Properties.Resources.knowledge_18514026__3_1;
+            this.button4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button4.Location = new System.Drawing.Point(0, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(244, 76);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "MOSTRAR APRENDIZAJE";
+            this.button4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.button5);
+            this.panel5.Location = new System.Drawing.Point(3, 435);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(244, 76);
+            this.panel5.TabIndex = 6;
+            // 
+            // button5
+            // 
+            this.button5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Font = new System.Drawing.Font("Cooper Black", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.Color.Red;
+            this.button5.Image = global::Piedra_Papel_Tijeras.Properties.Resources.icons8_salida_50;
+            this.button5.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button5.Location = new System.Drawing.Point(0, 0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(244, 76);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "VOLVER";
+            this.button5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Sidebar_Timer
+            // 
+            this.Sidebar_Timer.Interval = 10;
+            this.Sidebar_Timer.Tick += new System.EventHandler(this.Sidebar_Timer_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Location = new System.Drawing.Point(18, 18);
+            this.pictureBox1.MinimumSize = new System.Drawing.Size(50, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Padding = new System.Windows.Forms.Padding(8);
+            this.pictureBox1.Size = new System.Drawing.Size(65, 65);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panelTitulos
+            // 
+            this.panelTitulos.BackColor = System.Drawing.Color.Transparent;
+            this.panelTitulos.Controls.Add(this.label2);
+            this.panelTitulos.Controls.Add(this.label3);
+            this.panelTitulos.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTitulos.Location = new System.Drawing.Point(0, 0);
+            this.panelTitulos.Name = "panelTitulos";
+            this.panelTitulos.Size = new System.Drawing.Size(1321, 110);
+            this.panelTitulos.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.label2.Location = new System.Drawing.Point(155, 23);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(397, 60);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "APRENDIZAJE";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(66)))), ((int)(((byte)(133)))), ((int)(((byte)(180)))));
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(647, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(397, 60);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "JUGADAS APRENDIDAS: 0";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panelTabla
+            // 
+            this.panelTabla.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panelTabla.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelTabla.Controls.Add(this.dataGridView1);
+            this.panelTabla.Location = new System.Drawing.Point(306, 417);
+            this.panelTabla.Name = "panelTabla";
+            this.panelTabla.Padding = new System.Windows.Forms.Padding(14);
+            this.panelTabla.Size = new System.Drawing.Size(984, 258);
+            this.panelTabla.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.LightGray;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(66)))), ((int)(((byte)(133)))), ((int)(((byte)(180)))));
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle5.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGridView1.ColumnHeadersHeight = 60;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Segoe UI", 11F);
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(234)))), ((int)(((byte)(248)))));
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle6;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(225)))), ((int)(((byte)(225)))));
+            this.dataGridView1.Location = new System.Drawing.Point(14, 14);
+            this.dataGridView1.MultiSelect = false;
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 55;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(954, 228);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // panelContenido
+            // 
+            this.panelContenido.BackColor = System.Drawing.Color.Transparent;
+            this.panelContenido.BackgroundImage = global::Piedra_Papel_Tijeras.Properties.Resources.fondoTablav2;
+            this.panelContenido.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelContenido.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelContenido.Location = new System.Drawing.Point(0, 0);
+            this.panelContenido.Name = "panelContenido";
+            this.panelContenido.Size = new System.Drawing.Size(1321, 760);
+            this.panelContenido.TabIndex = 0;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Piedra_Papel_Tijeras.Properties.Resources.fondoTablav2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1391, 760);
+            this.Controls.Add(this.panelTabla);
+            this.Controls.Add(this.panelTitulos);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Sidebar);
+            this.Name = "Form3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Aprendizaje - Piedra, Papel o Tijeras";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Sidebar.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.MenuButton)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelTitulos.ResumeLayout(false);
+            this.panelTabla.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FlowLayoutPanel Sidebar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox MenuButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Timer Sidebar_Timer;
+        private System.Windows.Forms.Panel panelTitulos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panelTabla;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panelContenido;
+    }
+}
---
+++ Piedra_Papel_Tijeras/Form3.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace Piedra_Papel_Tijeras
+{
+    public partial class Form3 : Form
+    {
+        bool siderbarExpand; // Variable para controlar el estado del sidebar
+        private BD bd;
+        private PrivateFontCollection coleccionFuentes;
+
+
+        public Form3()
+        {
+            InitializeComponent();
+            bd = new BD();
+            this.Load += Form3_Load;
+            HabilitarDobleBufer(this);
+
+
+        }
+        private void HabilitarDobleBufer(Control control)
+        {
+            typeof(Control).GetProperty("DoubleBuffered",
+                System.Reflection.BindingFlags.NonPublic |
+                System.Reflection.BindingFlags.Instance)
+                ?.SetValue(control, true, null);
+
+            foreach (Control hijo in control.Controls)
+            {
+                HabilitarDobleBufer(hijo);
+            }
+        }
+        private async void Form3_Load(object sender, EventArgs e)
+        {
+
+            dataGridView1.DataSource = null;
+            var contadores = await bd.ObtenerContadoresAsync();
+            dataGridView1.DataSource = ConstruirMatrizAprendizaje(contadores);
+
+            int total = contadores.Piedra_Piedra + contadores.Piedra_Papel + contadores.Piedra_Tijera
+                      + contadores.Papel_Piedra + contadores.Papel_Papel + contadores.Papel_Tijera
+                      + contadores.Tijera_Piedra + contadores.Tijera_Papel + contadores.Tijera_Tijera;
+
+            label3.Text = $"JUGADAS APRENDIDAS: {total}";
+        }
+
+        private DataTable ConstruirMatrizAprendizaje(Contadores c)
+        {
+            var tabla = new DataTable();
+            tabla.Columns.Add("Jugada Anterior");
+            tabla.Columns.Add("-> PIEDRA");
+            tabla.Columns.Add("-> PAPEL");
+            tabla.Columns.Add("-> TIJERA");
+            tabla.Columns.Add("TOTAL");
+
+            AgregarFila(tabla, "PIEDRA", c.Piedra_Piedra, c.Piedra_Papel, c.Piedra_Tijera);
+            AgregarFila(tabla, "PAPEL", c.Papel_Piedra, c.Papel_Papel, c.Papel_Tijera);
+            AgregarFila(tabla, "TIJERA", c.Tijera_Piedra, c.Tijera_Papel, c.Tijera_Tijera);
+
+            return tabla;
+        }
+
+        private void AgregarFila(DataTable tabla, string jugadaAnterior, int p, int pa, int t)
+        {
+            int total = p + pa + t;
+            string Col(int valor) => total > 0 ? $"{valor} ({(valor * 100.0 / total):0.#}%)" : "0";
+
+            tabla.Rows.Add(jugadaAnterior, Col(p), Col(pa), Col(t), total);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            this.Visible = false;
+            form1.Visible = true;
+        }
+        private void Sidebar_Timer_Tick(object sender, EventArgs e)
+        {
+            //Controla el maximo y minimo del sidebar, expandiendolo o contrayendolo dependiendo de su estado
+            if (siderbarExpand)
+            {
+                Sidebar.Width -= 10;
+                if (Sidebar.Width == Sidebar.MinimumSize.Width)
+                {
+                    siderbarExpand = false;
+                    Sidebar_Timer.Stop();
+                }
+            }
+            else
+            {
+                Sidebar.Width += 10;
+                if (Sidebar.Width == Sidebar.MaximumSize.Width)
+                {
+                    siderbarExpand = true;
+                    Sidebar_Timer.Stop();
+                }
+            }
+        }
+
+        private void MenuButton_Click(object sender, EventArgs e)
+        {
+            Sidebar_Timer.Start();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form1 form1 = new Form1();
+            this.Visible = false;
+            form1.Visible = true;
+        }
+    }
+}
